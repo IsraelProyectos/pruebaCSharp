b7e1893bb890dca5ea0eb90034746beb26d180e1
IsraelPuyol <israelpuyol@hotmail.com>
2017-05-07 16:01:53 +0200

    a

--- pruebaCSharp/Form1.cs
+++ pruebaCSharp/Form1.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-		//hola
+		//hola israel
         public Form1()
         {
             InitializeComponent();
